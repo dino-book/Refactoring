c88bf30dc193de17dfbff4a9a50360f79ee54591
IndongLee <kundera@kakao.com>
2021-03-20 22:20:04 +0900

    html로 렌더링 하는 기능 추가.

--- Chapter01/src/main/kotlin/step02.kt
+++ Chapter01/src/main/kotlin/step02.kt
@@ -3,7 +3,15 @@ import kotlin.math.max
 import kotlin.math.round
 
 private fun statement(invoice: Invoice, plays: Map<String, Play>): String {
-    val statement = invoice.performanceSummaries.map { performanceSummaries ->
+    return renderPlainText(createStatement(invoice))
+}
+
+private fun htmlStatement(invoice: Invoice, plays: Map<String, Play>): String {
+    return renderHtml(createStatement(invoice))
+}
+
+private fun createStatement(invoice: Invoice): Statement {
+    return invoice.performanceSummaries.map { performanceSummaries ->
         enrichPerformance(performanceSummaries)
     }.let { performances ->
         Statement(
@@ -11,13 +19,11 @@ private fun statement(invoice: Invoice, plays: Map<String, Play>): String {
             performances,
             totalAmount(performances),
             totalVolumeCredits(performances)
-            )
+        )
     }
-
-    return renderPlainText(statement, plays)
 }
 
-private fun renderPlainText(statement: Statement, plays: Map<String, Play>): String {
+private fun renderPlainText(statement: Statement): String {
     var result = "청구 내역 (고객명 : ${statement.customer})\n"
 
     for (perf in statement.performances) {
@@ -30,26 +36,45 @@ private fun renderPlainText(statement: Statement, plays: Map<String, Play>): Str
     return result
 }
 
+private fun renderHtml(statement: Statement): String {
+    var result = "<h1>청구 내역 (고객명: ${statement.customer}</h1>\n"
+
+    result += "<table>\n"
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th>></tr>"
+
+    for (perf in statement.performances) {
+        result += " <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>"
+        result += "<td>${usd(perf.amount)}</td></tr>"
+    }
+    result += "</table>\n"
+
+    result += "<p>총액: <em>${usd(statement.totalAmount)}</em></p>\n"
+    result += "<p>적립 포인트: <em>${statement.totalVolumeCredits}</em>점</p>\n"
+    return result
+}
+
 private fun enrichPerformance(performanceSummary: PerformanceSummary): Performance {
+    val play = playFor(performanceSummary)!!
+
     return Performance(
         performanceSummary.playId,
-        playFor(performanceSummary)!!,
+        play,
         performanceSummary.audience,
-        amountFor(performanceSummary),
-        volumeCreditsFor(performanceSummary)
+        amountFor(performanceSummary, play.type),
+        volumeCreditsFor(performanceSummary, play.type)
     )
 }
 
 private fun totalAmount(performances: List<Performance>): Int {
-    return performances.sumOf { performances ->
-        performances.amount
+    return performances.sumOf { performance ->
+        performance.amount
     }
 }
 
-private fun amountFor(performance: PerformanceSummary): Int {
+private fun amountFor(performance: PerformanceSummary, playType: PlayType): Int {
     var result = 0
 
-    when (playFor(performance)!!.type) {
+    when (playType) {
         PlayType.TRAGEDY -> {
             result = 40_000
             if (performance.audience > 30) {
@@ -72,14 +97,14 @@ private fun playFor(performance: PerformanceSummary): Play? {
     return plays[performance.playId]
 }
 
-private fun volumeCreditsFor(performance: PerformanceSummary): Int {
+private fun volumeCreditsFor(performance: PerformanceSummary, playType: PlayType): Int {
     var volumeCredits = 0
 
     // 포인트를 적립한다.
     volumeCredits += max(performance.audience - 30, 0)
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if (playFor(performance)?.type == PlayType.COMEDY) {
+    if (playType == PlayType.COMEDY) {
         volumeCredits += performance.audience / 5
     }
 
