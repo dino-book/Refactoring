3d1e65656311724a01527e30d480589aac994b84
IndongLee <kundera@kakao.com>
2021-05-22 01:52:15 +0900

    6-2 함수 인라인하기

---
+++ Chapter06/02_함수_인라인하기/03.js
@@ -0,0 +1,10 @@
+function reportLInes(aCustomer) {
+    const lines = []
+    gatherCustomerData(lines, aCustomer)
+    return lines
+}
+
+function gatherCustomerData(out, aCustomer) {
+    out.push(["name", aCustomer.name])
+    out.push(["location", aCustomer.location])
+}
