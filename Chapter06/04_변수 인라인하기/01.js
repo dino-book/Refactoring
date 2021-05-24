e41d013748667c5a78684f5ef7c88afa66f8ce28
IndongLee <kundera@kakao.com>
2021-05-24 22:13:41 +0900

    6-4 변수 인라인하기

---
+++ Chapter06/04_변수 인라인하기/01.js
@@ -0,0 +1,4 @@
+function isBasePriceHigherThan1000() {
+    let basePrice = anOrder.basePrice
+    return (basePrice > 1000)
+}
---
+++ Chapter06/04_변수 인라인하기/02.js
@@ -0,0 +1,3 @@
+function isBasePriceHigherThan1000() {
+    return anOrder.basePrice > 1000
+}
