317f1ddaf3f3f4894847e3253cf20d2356117a90
IndongLee <kundera@kakao.com>
2021-05-27 06:08:15 +0900

    6-8 매개변수 객체 만들기

---
+++ Chapter06/08_매개변수 객체 만들기/02.js
@@ -0,0 +1,33 @@
+const station = {
+    name: "ZB1",
+    readings: [
+        {temp: 47, time: "2016-11-10 09:10"},
+        {temp: 53, time: "2016-11-10 09:20"},
+        {temp: 58, time: "2016-11-10 09:30"},
+        {temp: 53, time: "2016-11-10 09:40"},
+        {temp: 51, time: "2016-11-10 09:50"}
+    ]
+}
+
+function readingsOutsideRange(station, min, max, range) {
+    return station.readings.filter(r => r.temp < min || r.temp > max)
+}
+
+alerts = readingsOutsideRange(
+    station,
+    operatingPlan.temperatureFloor,
+    operatingPlan.temperatureCeiling,
+    null
+)
+
+class NumberRange {
+    constructor(min, max) {
+        this._data = {
+            min: min,
+            max: max
+        }
+    }
+
+    get min() {return this._data.min}
+    get max() {return this._data.max}
+}
---
+++ Chapter06/08_매개변수 객체 만들기/03.js
@@ -0,0 +1,29 @@
+const station = {
+    name: "ZB1",
+    readings: [
+        {temp: 47, time: "2016-11-10 09:10"},
+        {temp: 53, time: "2016-11-10 09:20"},
+        {temp: 58, time: "2016-11-10 09:30"},
+        {temp: 53, time: "2016-11-10 09:40"},
+        {temp: 51, time: "2016-11-10 09:50"}
+    ]
+}
+
+function readingsOutsideRange(station, range) {
+    return station.readings.filter(r => r.temp < range.min || r.temp > range.max)
+}
+
+const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling)
+alerts = readingsOutsideRange(station, range)
+
+class NumberRange {
+    constructor(min, max) {
+        this._data = {
+            min: min,
+            max: max
+        }
+    }
+
+    get min() {return this._data.min}
+    get max() {return this._data.max}
+}
